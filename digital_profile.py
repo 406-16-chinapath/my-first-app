128c79ce9442bbec29f53c639d6b2ae2f53a8f09
406-16-chinapath <54390@yupparaj.ac.th>
2026-06-26 10:35:43 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My Digital Profile")
+
+st.header("แนะนำตัว")
+st.write("นายชิณภัทร อนุวงค์")
+st.write("ชื่อเล่น ธันวา")
+st.write("เลขที่16 ม.4/6")
+
+st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย:ตั้งใจเรียนให้ได้เกรด4")
+st.write("สิ่งที่อยากเป็นในอนาคต:วิศวกร")
